00cda55fc17b8088adf0be359d3cc77a98ec6e55
mero mero <mero20261@gmail.com>
2020-07-17 14:40:29 +0200

    removed Angular temporary template & created addTodo and todoList pages & added new pages routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
+import { AddTodoComponent } from './pages/add-todo/add-todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'AddItem',component:AddTodoComponent},
+  {path:'',component:TodoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
